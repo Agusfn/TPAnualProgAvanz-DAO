7c0e05bc99ce54c1943e198095d5bb0d3f84317c
ianmi <ianmi@DESKTOP-2DJKAIL>
2019-06-17 17:14:17 -0300

    se ingresaron las clases de Aerolinea y Cliente en implementations e
    interfaces

---
+++ TPAnualDAO/src/dao/interfaces/IClienteDao.java
@@ -0,0 +1,18 @@
+package dao.interfaces;
+
+import java.io.IOException;
+import java.util.List;
+
+import modelo.Cliente;
+
+
+public interface IClienteDao {
+
+	public Cliente obtener(int id) throws IOException;
+	public List<Cliente> obtenerTodos() throws IOException;	
+	public void agregar(Cliente cliente) throws IOException;
+	public void eliminar(Cliente cliente) throws IOException;
+	public void actualizar(Cliente cliente) throws IOException;
+	
+	
+}
