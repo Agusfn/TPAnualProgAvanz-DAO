764abf358b63598ea4728b1036b1a094241b2853
Agustin FN Laptop <agusfn20@gmail.com>
2019-06-18 20:06:37 -0300

    Agregado factory pais.

--- TPAnualDAO/src/dao/factory/PaisFactory.java
+++ TPAnualDAO/src/dao/factory/PaisFactory.java
@@ -1,5 +1,22 @@
 package dao.factory;
 
+import java.io.IOException;
+
+import dao.implementations.PaisDaoImplArchivo;
+import dao.interfaces.IPaisDao;
+
 public class PaisFactory {
 
+	public static IPaisDao getImplementation (String source) throws IOException {
+		
+		if (source.equals(("archivo"))) {
+			return new PaisDaoImplArchivo();
+		}
+		/*else if (source.equals("serializacion")) {	
+			return new PaisDaoImplSerializacion();
+		}*/
+		return null;
+		
+	}	
+	
 }
