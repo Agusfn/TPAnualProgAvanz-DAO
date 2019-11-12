a8b55a9c05e12b2f5bb1f83e2ab836fca67b00c2
Agustin FN <agusfn20@gmail.com>
2019-11-12 15:01:41 -0300

    Fix.

--- TPAnualDAO/src/dao/interfaces/IAerolineaDao.java
+++ TPAnualDAO/src/dao/interfaces/IAerolineaDao.java
@@ -1,6 +1,5 @@
 package dao.interfaces;
 
-import java.sql.SQLException;
 import java.util.List;
 
 import modelo.Aerolinea;
--- TPAnualDAO/src/maintest/Main.java
+++ TPAnualDAO/src/maintest/Main.java
@@ -1,78 +1,10 @@
 package maintest;
 
-import java.util.List;
-
-import dao.implementations.archivo.PaisDaoImplArchivo;
-import dao.implementations.db.PaisDaoImplDB;
-import dao.implementations.db.ProvinciaDaoImplDB;
-import modelo.Pais;
-import modelo.Provincia;
-import util.DbQuery;
-import util.Properties;
 
 public class Main {
 
 	public static void main(String[] args) throws Exception
 	{
-		
-		
-		
-		
-		
-		
-		/*PaisDaoImplArchivo daoPais = new PaisDaoImplArchivo();
-		
-		
-		
-		Pais pais = daoPais.obtener(1);
-		
-		System.out.println(pais.getNombre());*/
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		/*DbQuery query = new DbQuery();
-		
-		ProvinciaDaoImplDB provinciaDao = new ProvinciaDaoImplDB(query);
-		PaisDaoImplDB paisDao = new PaisDaoImplDB(query);
-		
-		
-		Pais pais = paisDao.obtener(1);
-		
-		Provincia provincia = new Provincia();
-		provincia.setNombre("Neuquén");
-		provincia.setPais(pais);
-		
-		provinciaDao.agregar(provincia);*/
-		
-		/*provincia.setPais(paisDao.obtener(provincia.getPais().getId()));
-		
-		System.out.println(provincia.getPais().getNombre());*/
-		
-		
-		
-		
-		
-		//query.close();
-		
-		
-		//List<Pais> paises = paisDao.obtenerTodos();
-		
-		/*for(Pais pais: paises) {
-			System.out.println(pais.getNombre());
-		}*/
-		
-		/*paisDao.agregar(pais);
-		
-		*/
-		
-		
 	}
 	
 	
